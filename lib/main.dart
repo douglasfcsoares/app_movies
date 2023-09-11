063d3779902c30ffb5a151925d7da7670d143f91
douglasfcsoares <douglasfcsoares@gmail.com>
2023-09-11 17:17:46 -0300

    adicionei imagens ao assets e a chave sha1 ao firebase

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,9 @@ import 'package:app_movies/modules/splash/splash_module.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 
+//? Figma do projeto.
+//? https://www.figma.com/file/uxIIdBQowPYx5KS9nGHS4A/Filmes?type=design&node-id=4-1304&mode=design&t=kzGl0wsu4YZL0jGG-0
+
 void main() {
   runApp(const MyApp());
 }
@@ -12,6 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
